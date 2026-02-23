606fc5ed4d80d9ccfa53316bcd4a9cad5b9d8d44
Valentin Lindorfer <valentin.lindorfer@spyteam.one>
2026-02-23 23:28:08 +0100

    added member bias to Neuron.py

--- Neuron.py
+++ Neuron.py
@@ -1,5 +1,7 @@
 class Neuron:
-    def __init__(self, previous_neuron_weights: list, next_neuron_weights: list):
+    def __init__(self, previous_neuron_weights: list, next_neuron_weights: list, bias):
         self.previous_neuron_weights = previous_neuron_weights
         self.next_neuron_weights = next_neuron_weights
+        self.bias = bias
+
         self.activation = 0.0
