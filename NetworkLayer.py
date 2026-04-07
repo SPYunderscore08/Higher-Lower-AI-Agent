e087e80e364c1334d932d6a877c6760d52f21ab7
Valentin Lindorfer <valentin.lindorfer@spyteam.one>
2026-04-08 00:24:54 +0200

    added weights in matrix form; made weights matrices accessible within each Network Layer; ensured connections between Layers via weight matrices

--- NetworkLayer.py
+++ NetworkLayer.py
@@ -2,4 +2,8 @@ from Neuron import *
 
 class NetworkLayer:
     def __init__(self, size: int):
+        self.size = size
+
+        self.prev_weight_matrix = None
+        self.next_weight_matrix = None
         self.neurons = [Neuron() for _ in range(size)]
--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,3 +1,5 @@
+import random
+
 from Neuron import *
 from NetworkLayer import *
 
@@ -6,6 +8,24 @@ class NeuralNetwork:
         self.input_layer = NetworkLayer(number_of_inputs)
         self.hidden_layers = [NetworkLayer(hidden_layer_size) for _ in range(number_of_hidden_layers)]
         self.output_layer = NetworkLayer(number_of_outputs)
+        self.assign_weight_matrices()
+
+    def assign_weight_matrices(self):
+        tmp = self.generate_weight_matrix(self.input_layer.size, self.hidden_layers[0].size)
+        self.input_layer.next_weight_matrix = tmp
+        self.hidden_layers[0].prev_weight_matrix = tmp
+
+        for i in range(1, len(self.hidden_layers)):
+            tmp = self.generate_weight_matrix(self.hidden_layers[i].size, self.hidden_layers[i - 1].size)
+            self.hidden_layers[i - 1].next_weight_matrix = tmp
+            self.hidden_layers[i].prev_weight_matrix = tmp
+
+        tmp = self.generate_weight_matrix(self.hidden_layers[-1].size, self.output_layer.size)
+        self.hidden_layers[-1].next_weight_matrix = tmp
+        self.output_layer.prev_weight_matrix = tmp
+
+    def generate_weight_matrix(self, rows: int, columns: int):
+        return list([random.random() for _ in range(columns)] for _ in range(rows))
 
     def calculate_result(self, input_list: list):
         pass
